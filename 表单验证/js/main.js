515a8e4c66c8f6ba832f5393644258db0d3fe36f
youngrivers <401712228@qq.com>
2016-09-26 10:37:19 +0800

    表单验证

---
+++ 表单验证/js/main.js
@@ -0,0 +1,31 @@
+/**
+ * Created by youngrivers on 2016/9/26.
+ */
+angular.module('myApp',[])
+.controller('SignUpController',function ($scope) {
+    $scope.userdata={};
+    $scope.submitForm=function () {
+        console.log($scope.userdata)
+        if($scope.signUpForm.$invalid)
+            alert("提交错误")
+        else
+            alert("提交成功")
+    }
+})
+.directive('compare',function () {
+    var o=[];
+    o.strict='AE';
+    o.scope={
+        orgText:'=compare'
+    };
+    o.require='ngModel';
+    o.link=function (sco, ele, att, con) {
+        con.$validators.compare=function (v) {
+            return v==sco.orgText;
+        }
+        sco.$watch('orgText',function () {
+            con.$validate();
+        })
+    }
+    return o;
+})
